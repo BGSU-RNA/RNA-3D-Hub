636a7233fda62258ff9869f1f9bf4c62435a5e92
Anton Petrov <anton.i.petrov@gmail.com>
2011-11-25 22:43:30 -0500

    Load same menu_view in the home page

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,6 +6,8 @@ class Home extends CI_Controller {
         $data['title'] = 'All Motif Atlas releases';
         $data['baseurl'] = base_url();
 
+        $this->load->view('header_view', $data);
+        $this->load->view('menu_view', $data);
         $this->load->view('home_view', $data);
 	}
 
