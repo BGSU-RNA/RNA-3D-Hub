20b9ee6d44aa77e3c44b5bb701c98f7dc8fa97c5
Anton Petrov <anton.i.petrov@gmail.com>
2012-01-31 17:25:53 -0500

    Add "RNA Loops" to navigation

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -8,13 +8,20 @@
                 <a href="#" class="dropdown-toggle">RNA 3D Structures</a>
                 <ul class="dropdown-menu">
                     <li><a href="<?php echo $baseurl;?>pdb">View all PDBs</a></li>
-                    <li><a href="<?php echo $baseurl;?>loops">View loop stats</a></li>
-                    <li class="divider"></li>
-                    <li><a href="<?php echo $baseurl;?>loops/benchmark/IL">Loop extraction benchmark</a></li>
                     <li class="divider"></li>
                     <li><a href="<?php echo $baseurl;?>loops/sfdata">Sfcheck and Mapman: Loops</a></li>
                     <li><a href="<?php echo $baseurl;?>loops/sfjmol">Sfcheck and Mapman: PDBs</a></li>
                     <li><a href="<?php echo $baseurl;?>loops/graphs">Sfcheck and Mapman: Graphs</a></li>
+                </ul>
+            </li>
+
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle">RNA Loops</a>
+                <ul class="dropdown-menu">
+                    <li><a href="<?php echo $baseurl;?>loops">Weekly stats</a></li>
+                    <li><a href="<?php echo $baseurl;?>pdb">Browse by PDB</a></li>
+                    <li class="divider"></li>
+                    <li><a href="<?php echo $baseurl;?>loops/benchmark/IL">Loop extraction benchmark</a></li>
                 </ul>
             </li>
 
