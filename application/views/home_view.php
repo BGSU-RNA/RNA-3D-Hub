e23dd868447b14763c0e47f2878a0a1894754788
Anton Petrov <anton.i.petrov@gmail.com>
2012-11-13 16:58:12 -0500

    Home page tweaks

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -5,28 +5,39 @@
 
       <!-- Main hero unit for a primary marketing message or call to action -->
       <div class="hero-unit">
-        <h1>RNA 3D Hub</h1>
-        <p>The single-stop destination for RNA structural annotations</p>
-        <p>Public &szlig; release</p>
+        <h1>RNA 3D Hub<small>Public &szlig; release</small></h1>
+        <h3>The single-stop destination for RNA structural annotations</h3>
+        <br>
+        <a href="http://rna.bgsu.edu/main/introduction-to-rna-3d-hub/" class="btn success large">Introduction</a>
+
       </div>
 
       <div class="row">
 
-        <div class="span5">
+        <div class="span6">
           <h2>RNA Structure Atlas</h2>
-          <p>Structural annotations of all RNA-containing 3D structures. Pairwise interactions produced by <a href="http://rna.bgsu.edu/FR3D">FR3D</a>, hairpin, internal and three-way junction loops, motif annotations from the <a href="http://rna.bgsu.edu/rna3dhub/motifs">Motif Atlas</a>. Automatic updates.</p>
+          Structural annotations of all RNA-containing 3D structures. Main features:
+          <ul>
+             <li>Pairwise interactions produced by <a href="http://rna.bgsu.edu/FR3D">FR3D</a></li>
+             <li>Hairpin, internal, and three-way junction loops</li>
+             <li>Motif annotations from the <a href="http://rna.bgsu.edu/rna3dhub/motifs">Motif Atlas</a></li>
+             <li>Similar structures from the <a href="http://rna.bgsu.edu/rna3dhub/nrlist">Non-redundant Atlas</a></li>
+             <li>Automatic updates</li>
+          </ul>
           <p><a class="btn primary" href="<?php echo $baseurl;?>pdb">Explore</a></p>
         </div>
 
-        <div class="span6">
+        <div class="span5">
           <h2>RNA 3D Motif Atlas</h2>
-          <p>A representative collection of recurrent RNA 3D internal and hairpin loop motifs extracted from a non-redundant set of structures. Automatic updates coming soon!</p>
+          <p>A representative collection of recurrent RNA 3D internal and hairpin loop motifs extracted from a non-redundant set of structures.</p>
+          <p>Automatic updates coming soon!</p>
           <p><a class="btn primary" href="<?php echo $baseurl;?>motifs">Explore</a></p>
         </div>
 
         <div class="span5">
           <h2>Non-redundant Atlas</h2>
-          <p>PDB files grouped into equivalence classes by molecule type, organism, and resolution. Automatic weekly updates.</p>
+          <p>PDB files grouped into equivalence classes by molecule type, organism, and resolution.</p>
+          <p>Automatic weekly updates.</p>
           <p><a class="btn primary" href="<?php echo $baseurl;?>nrlist">Explore</a></p>
        </div>
 
