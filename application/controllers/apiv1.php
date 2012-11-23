2b4233aa81dbc13cb5fa766d115fbc6b3e98ecdf
Anton Petrov <anton.i.petrov@gmail.com>
2012-11-23 10:44:47 -0500

    Begin work on RNA 3D Hub api v1
    - create a new controller
    - make use of the existing models
    - this api can be used by R3DAlign and other web applications without having to connect to the database or reproducing the queries.

---
+++ application/controllers/apiv1.php
@@ -0,0 +1,42 @@
+<?php
+class Apiv1 extends CI_Controller {
+
+	public function index()
+	{
+        show_404();
+	}
+
+    public function get_all_rna_pdb_ids()
+    {
+        $this->load->model('Pdb_model', '', TRUE);
+        $pdbs = $this->Pdb_model->get_all_pdbs();
+
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode(array('pdb_ids' => $pdbs)));
+    }
+
+    public function get_structure_info($pdb_id)
+    {
+        $this->load->model('Pdb_model', '', TRUE);
+        $data = $this->Pdb_model->get_general_info($pdb_id);
+
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data));
+    }
+
+    public function get_equivalent_structures($pdb_id)
+    {
+        $this->load->model('Pdb_model', '', TRUE);
+        $data = $this->Pdb_model->get_related_structures($pdb_id);
+
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($data));
+    }
+
+}
+
+/* End of file pdb.php */
+/* Location: ./application/controllers/pdb.php */
--- application/models/pdb_model.php
+++ application/models/pdb_model.php
@@ -300,6 +300,10 @@ class Pdb_model extends CI_Model {
                  $row->entityMacromoleculeType == 'DNA/RNA Hybrid' ) {
                 $data['rna_chains']++;
                 $organisms[] = $row->source;
+                $data['rna_compounds'][] = array("chain"    => $row->chainId,
+                                                 "compound" => $row->compound,
+                                                 "length"   => $row->chainLength,
+                                                 "organism" => $row->source);
             }
             // for all chains
             $compounds[] = $row->compound;
@@ -433,7 +437,7 @@ class Pdb_model extends CI_Model {
 
             foreach($query->result() as $row) {
                 if ( $row->id != $pdb_id ) {
-                    $pdbs[] = "<a class='pdb'>{$row->id}</a>";
+                    $pdbs[] = $row->id;
                 }
             }
         }
