38a9dd249989cd711261acdc0b9c4b5438aca334
Anton Petrov <anton.i.petrov@gmail.com>
2013-03-13 15:27:19 -0400

    Change text on the home page

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -16,21 +16,21 @@
 
         <div class="span6">
           <h2>RNA Structure Atlas</h2>
-          Structural annotations of all RNA-containing 3D structures. Main features:
+          Structural annotations of all RNA-containing 3D structures including:
           <ul>
              <li>Pairwise interactions produced by <a href="http://rna.bgsu.edu/FR3D">FR3D</a></li>
              <li>Hairpin, internal, and three-way junction loops</li>
              <li>Motif annotations from the <a href="http://rna.bgsu.edu/rna3dhub/motifs">Motif Atlas</a></li>
              <li>Similar structures from the <a href="http://rna.bgsu.edu/rna3dhub/nrlist">Non-redundant Lists</a></li>
-             <li>Automatic updates</li>
           </ul>
+          <p>Automatic weekly updates.</p>
           <p><a class="btn primary" href="<?php echo $baseurl;?>pdb">Explore</a></p>
         </div>
 
         <div class="span5">
           <h2>RNA 3D Motif Atlas</h2>
           <p>A representative collection of recurrent RNA 3D internal and hairpin loop motifs extracted from a non-redundant set of structures.</p>
-          <p>Automatic updates coming soon!</p>
+          <p>Automatic updates every 4 weeks.</p>
           <p><a class="btn primary" href="<?php echo $baseurl;?>motifs">Explore</a></p>
         </div>
 
