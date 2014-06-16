b8aee4307ba21b78e8f43438f56789ca3cc3f7f0
Blake Sweeney <blakes.85@gmail.com>
2014-06-16 15:00:28 -0400

    Fix another short tag.

--- application/views/menu_view.php
+++ application/views/menu_view.php
@@ -97,7 +97,7 @@
                 </ul>
             </li>
 
-            <li><a href="<?=$baseurl?>search"><span class="label success">New!</span> Search</a></li>
+            <li><a href="<?php echo $baseurl?>search"><span class="label success">New!</span> Search</a></li>
 
           </ul>
             <ul class="nav secondary-nav">
