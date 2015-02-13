c7f2184439be0bc5b81dc67b18c8bf1c0de91155
Anton Petrov <anton.i.petrov@gmail.com>
2015-02-13 13:29:48 -0500

    Use Jsmol on individual loop pages

--- application/views/loops_single_view.php
+++ application/views/loops_single_view.php
@@ -120,11 +120,41 @@
             </div>
 
             <div class="span6 well" id="jmol" >
-                <script type="text/javascript">
-                    jmolInitialize(" /jmol");
-                    jmolSetAppletColor("#f5f5f5");
-                    jmolApplet(340, "javascript appletLoaded()");
-                </script>
+<script>
+    jmol_isReady = function(applet) {
+        // initialize the plugin
+        $('.jmolInline').jmolTools({
+            showStereoId: 'stereo',
+            showNeighborhoodId: 'neighborhood',
+            showNumbersId: 'showNtNums',
+        });
+        // run the plugin
+        $('.jmolInline').first().jmolToggle();
+    };
+
+    var Info = {
+        width: 340,
+        height: 340,
+        debug: false,
+        color: '#f5f5f5',
+        addSelectionOptions: false,
+        use: 'HTML5',
+        j2sPath: '<?=$baseurl?>/js/jsmol/j2s/',
+        readyFunction: jmol_isReady,
+        disableInitialConsole: true
+    };
+
+    var jmolApplet0 = Jmol.getApplet('jmolApplet0', Info);
+
+    // these are conveniences that mimic behavior of Jmol.js
+    function jmolCheckbox(script1, script0,text,ischecked) {Jmol.jmolCheckbox(jmolApplet0,script1, script0, text, ischecked)};
+    function jmolButton(script, text) {Jmol.jmolButton(jmolApplet0, script,text)};
+    function jmolHtml(s) { document.write(s) };
+    function jmolBr() { jmolHtml("<br />") };
+    function jmolMenu(a) {Jmol.jmolMenu(jmolApplet0, a)};
+    function jmolScript(cmd) {Jmol.script(jmolApplet0, cmd)};
+    function jmolScriptWait(cmd) {Jmol.scriptWait(jmolApplet0, cmd)};
+</script>
                 <input type='button' id='neighborhood' class='btn' value="Show neighborhood">
                 <label><input type="checkbox" id="showNtNums">Nucleotide numbers</label>
                 <br><br>
@@ -138,21 +168,6 @@
 
 
       <script>
-
-
-        // initialize jmolTools
-        $('.jmolInline').jmolTools({
-            showStereoId: 'stereo',
-            showNeighborhoodId: 'neighborhood',
-            showNumbersId: 'showNtNums',
-        });
-
-        // run when jmol is ready
-      	function appletLoaded (){
-      	    // toggle the first checkbox
-      	    $('.jmolInline').first().jmolToggle();
-      	}
-
         $('#sortable').tablesorter();
 
         var offset_left = $('#left_content').offset().left + 530; // 530 = span9 width
