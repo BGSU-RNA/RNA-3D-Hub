fe8083bdf02695c1216e30992645d6d5bdab8909
Anton Petrov <anton.i.petrov@gmail.com>
2015-02-13 17:53:58 -0500

    Update the homepage
    * link to the blog post
    * inform about the coming update

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -7,7 +7,14 @@
       <div class="hero-unit">
         <h1>RNA 3D Hub</h1>
         <br>
+        <p>
         <a href="http://rna.bgsu.edu/main/introduction-to-rna-3d-hub/" class="btn success large">Introduction</a>
+        </p>
+        <p>
+          We are <a href="http://rna.bgsu.edu/main/upgrading-rna-3d-hub/">upgrading RNA 3D Hub</a>
+          to include new RNA 3D structures distributed in <a href="http://wwpdb.org/news/news.php?year=2014#10-December-2014">mmCIF format</a>.
+          Follow us on <a href="https://twitter.com/rna3dhub">Twitter</a> to hear when the updated version becomes available.
+        </p>
 
       </div>
 
@@ -22,21 +29,21 @@
              <li>Motif annotations from the <a href="http://rna.bgsu.edu/rna3dhub/motifs">Motif Atlas</a></li>
              <li>Similar structures from the <a href="http://rna.bgsu.edu/rna3dhub/nrlist">Non-redundant Lists</a></li>
           </ul>
-          <p>Automatic weekly updates.</p>
+          <p>Automatic weekly updates <a href="http://rna.bgsu.edu/main/upgrading-rna-3d-hub/">will resume soon</a></p>
           <p><a class="btn primary" href="<?php echo $baseurl;?>pdb">Explore</a></p>
         </div>
 
         <div class="span5">
           <h2>RNA 3D Motif Atlas</h2>
           <p>A representative collection of recurrent RNA 3D internal and hairpin loop motifs extracted from a non-redundant set of structures.</p>
-          <p>Automatic updates every 4 weeks.</p>
+          <p>Automatic updates <a href="http://rna.bgsu.edu/main/upgrading-rna-3d-hub/">will resume soon</a></p>
           <p><a class="btn primary" href="<?php echo $baseurl;?>motifs">Explore</a></p>
         </div>
 
         <div class="span5">
           <h2>Non-redundant Lists</h2>
           <p>PDB files grouped into equivalence classes by molecule type, organism, and resolution.</p>
-          <p>Automatic weekly updates.</p>
+          <p>Automatic updates <a href="http://rna.bgsu.edu/main/upgrading-rna-3d-hub/">will resume soon</a></p>
           <p><a class="btn primary" href="<?php echo $baseurl;?>nrlist">Explore</a></p>
        </div>
 
