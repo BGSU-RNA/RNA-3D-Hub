357d1884717709600444ba90e7299c56e06a1d49
Jamie Cannone <jjcannone@bluehen.udel.edu>
2016-11-15 16:32:12 -0600

    Add debugs to inner function
    (cherry picked from commit 564ebead3dcba3b417925ba5d81911171b58f144)

--- js/rna2d.js
+++ js/rna2d.js
@@ -1501,6 +1501,12 @@
 
         offset += chainBreakSize + angleSize * getNTData(chain).length;
 
+        console.log("--chain:", chain);
+        console.log("--chainIndex:", chainIndex);
+        console.log("--startAngle:", startAngle);
+        console.log("--endAngle:", endAngle);
+        console.log("--offset:", offset);
+
         return d3.svg.arc()
           .innerRadius(inner)
           .outerRadius(outer)
