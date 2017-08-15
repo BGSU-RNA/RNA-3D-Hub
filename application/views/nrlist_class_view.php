e444a8b8fdaa44362f01318bf3ce43ce091100c7
Sri Devan <sria@bgsu.edu>
2017-08-15 13:41:44 -0400

    Made some changes to the heat map js code
    Fixed the formatting of the js code and made the max discrepancy value
    displayed in the scale to be 0.5

--- application/views/nrlist_class_view.php
+++ application/views/nrlist_class_view.php
@@ -82,7 +82,7 @@
                  <div class="row span16 interactions resizable" id="statistics">
                   <div class="span16">
                       <div>
-                        <p align="center">Heat map of mutual geometric discrepancy, in Angstroms per nucleotide.  Instances are ordered to put similar structures near each other.</p>
+                        <p align="center">Heat map of mutual geometric discrepancy, in Angstroms per nucleotide.  Instances are ordered to put similar structures near each other. The colorbar ranges from 0 to the maximum observed discrepancy, up to 0.5</p>
                         <!-- Not sure what this is -->
                         <div id ='chart' style="text-align: center;"></div>
 
