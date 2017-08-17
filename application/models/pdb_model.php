8eda36f319849d1a70688d78e4aa21c8d09c8119
Jamie Cannone <jjcannone@bluehen.udel.edu>
2017-08-17 11:13:50 -0500

    Interim update for get_airport Querying the VIEW ss_unit_positions did not perform well.  This version replaces that query with one based upon the VIEW definition that performs better in initial testing.
    Currently throws two PHP errors, but I want to confirm that basic
    operations work for a few new structures before I fix those
    broken variable defintions.
    
    Conflicts:
    	application/models/pdb_model.php
    
    Fixed conflict.

--- application/models/pdb_model.php
+++ application/models/pdb_model.php
@@ -578,11 +578,43 @@ class Pdb_model extends CI_Model {
 
         $query = $this->db->get();
 
-        $nts_data = array();
-        $new_json = array();
-        $new_result = '';
-        $model = '';
-        $create = 0;
+        if ($query->num_rows()) {
+            // process ss_unit_positions
+
+            /*
+            //
+            //  First attempt to hack the new version, using ss_unit_positions.
+            //  This at least executes, even though I'm not doing anything
+            //    with the results.
+            //
+            $this->db->select()
+                     ->from('ss_unit_positions')
+                     ->where('pdb_id', $pdb_id);
+            */
+
+            //  Revision:  performance of view ss_unit_positions is horrible, but
+            //    the underlying query appears to perform better.
+            $this->db->select('UI.unit_id, SPM.pdb_id, UI.model, SPM.chain_name, UI.number')
+                     ->select('UI.unit, UI.alt_id, UI.ins_code, UI.sym_op, UI.chain_index')
+                     ->select('UI.unit_type_id, SP.index, SP.ss_id, SP.x_coordinate')
+                     ->select('SP.y_coordinate')
+                     ->select("IF(UI.unit_id IS NOT NULL, 1, 0) AS 'is_resolved'",false)
+                     ->from('ss_pdb_mapping AS SPM')
+                     ->join('ss_exp_seq_position_mapping AS ESPM', 'ESPM.ss_exp_seq_mapping_id = SPM.ss_exp_seq_mapping_id')
+                     ->join('ss_positions AS SP', 'SP.ss_position_id = ESPM.ss_position_id','left')
+                     ->join('exp_seq_unit_mapping AS ESUM','ESUM.exp_seq_position_id = ESPM.exp_seq_position_id','left')
+                     ->join('unit_info AS UI', 'UI.unit_id = ESUM.unit_id', 'left')
+                     ->where('ISNULL(UI.pdb_id)')
+                     ->or_where('UI.pdb_id = SPM.pdb_id')
+                     ->where('SPM.pdb_id', $pdb_id)
+                     ->group_by('SP.ss_position_id');
+;
+            $query = $this->db->get();
+
+            $nts_data = array();
+            $new_json = array();
+            $model = '';
+            $create = 0;
 
         foreach ($query->result() as $row) {
           $create = 1;
