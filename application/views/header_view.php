bcd130cef44da646e67a0caa651bf2a37bb10fb5
Craig L. Zirbel <zirbel@bgsu.edu>
2018-07-13 16:46:28 -0400

    Hard code R icon to see if it works

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -57,6 +57,8 @@
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="<?=$pageicon?>">
+    <link rel="shortcut icon" href="<?=$baseurl?>icons/R_icon.png">
+
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
