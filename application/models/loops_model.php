1e6f63244f21a58ec75c0149c4f24574af95d1a3
Jamie J. Cannone <jjcannone@bluehen.udel.edu>
2018-10-11 16:54:19 -0500

    Required ORDER BY clause

--- application/models/loops_model.php
+++ application/models/loops_model.php
@@ -94,7 +94,8 @@ class Loops_model extends CI_Model {
                          ->where('chain', $chain)
                          ->where('number >=', $beg)
                          ->where('number <=', $end)
-                         ->where_in('unit_id', $unit_split);
+                         ->where_in('unit_id', $unit_split)
+                         ->order_by('chain_index ASC');
                 $query2 = $this->db->get();
 
                 $tempres = array();
