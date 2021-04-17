649741114da09f79bbbada7a7dbcb276e3423f59
sridevan <sria@bgsu.edu>
2021-04-17 19:04:46 -0400

    Added a new feature to get the correspondence for unitids of interest

--- application/controllers/rest.php
+++ application/controllers/rest.php
@@ -144,6 +144,27 @@ class Rest extends MY_Controller {
 
     }
 
+    public function PairwiseInteractions()
+    {
+
+        // search POST, then GET
+        $query = $this->input->get_post('units');
+        
+        $this->output->enable_profiler(TRUE);
+        
+        if ( $query ) {
+            $this->load->model('Ajax_model', '', TRUE);
+
+            $this->output->set_header("Access-Control-Allow-Origin: *");
+            $this->output->set_header("Access-Control-Expose-Headers: Access-Control-Allow-Origin");
+            $data['json'] = $this->Ajax_model->get_pairwise_interactions($query);
+            $this->load->view('json_view', $data);
+        } else {
+            echo $this->messages['invalid'];
+        }
+
+    }
+
 
     public function getRSR()
     {
