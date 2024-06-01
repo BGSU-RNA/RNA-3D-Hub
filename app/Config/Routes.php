92769ced000ccf4e6bbc6464905454ed866e199b
Craig L. Zirbel <zirbel@bgsu.edu>
2024-06-01 00:32:02 -0400

    First configuration

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,18 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
+use App\Controllers\unitid;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index');
+// From https://www.codeigniter.com/user_guide/tutorial/news_section.html#display-the-news
+use App\Controllers\News; // Add this line
+
+// From https://www.codeigniter.com/user_guide/tutorial/static_pages.html
+use App\Controllers\Pages;
+
+$routes->get('unitid', [unitid::class, 'index']);
+$routes->get('unitid/describe/(:segment)', [unitid::class, 'describe']);
+
+$routes->get('news', [News::class, 'index']);           // Add this line
+$routes->get('news/(:segment)', [News::class, 'show']); // Add this line
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
