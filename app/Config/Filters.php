ab0eea4d5ce362bf03ae967ff1dc828bbb038f86
Craig L. Zirbel <zirbel@bgsu.edu>
2024-09-05 19:03:03 -0400

    disableDebug

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -34,6 +34,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'disableDebug' => \App\Filters\DisableDebugFilter::class, // added 2024-07-08
     ];
 
     /**
